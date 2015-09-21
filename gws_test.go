f2d841c5d2811c67bf8d563b84ee1adc3f326c07
Prateek Srivastava <f2prateek@gmail.com>
2015-09-21 00:20:13 -0700

    Add test for Pull Request Event

--- gws_test.go
+++ gws_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,37 +13,74 @@ import (
 	"github.com/google/go-github/github"
 )
 
-type Fixture struct {
-	Event     string          `json:"event"`
-	Signature string          `json:"signature"`
-	Payload   json.RawMessage `json:"payload"`
-}
-
-func TestServer(t *testing.T) {
-	s := New("")
-	ts := httptest.NewServer(s)
-	client := &http.Client{}
+func TestPushEvent(t *testing.T) {
+	s, ts := newTestServer("")
 	defer ts.Close()
-
-	data, err := ioutil.ReadFile("fixtures/push.json")
-	assert.Equal(t, nil, err)
-
-	var fixture Fixture
-	json.Unmarshal(data, &fixture)
-
-	requestBody := bytes.NewReader(fixture.Payload)
-	req, err := http.NewRequest("POST", ts.URL, requestBody)
-	req.Header.Add("X-Github-Event", fixture.Event)
-	assert.Equal(t, nil, err)
 
 	var event *github.PushEvent
 	go func() {
 		event = <-s.PushEvents
 	}()
 
-	resp, err := client.Do(req)
-	assert.Equal(t, nil, err)
+	resp := post(ts.URL, "push")
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	assert.NotEqual(t, nil, event)
+}
+
+func TestPullRequestEvent(t *testing.T) {
+	s, ts := newTestServer("")
+	defer ts.Close()
+
+	var event *github.PullRequestEvent
+	go func() {
+		event = <-s.PullRequestEvents
+	}()
+
+	resp := post(ts.URL, "pull_request")
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.NotEqual(t, nil, event)
+}
+
+func newTestServer(secret string) (*Server, *httptest.Server) {
+	s := New(secret)
+	ts := httptest.NewServer(s)
+	return s, ts
+}
+
+func post(url, file string) *http.Response {
+	data, err := ioutil.ReadFile("fixtures/" + file + ".json")
+	check(err)
+
+	var fixture struct {
+		Event     *string         `json:"event"`
+		Signature *string         `json:"signature"`
+		Payload   json.RawMessage `json:"payload"`
+	}
+	err = json.Unmarshal(data, &fixture)
+	check(err)
+
+	requestBody := bytes.NewReader(fixture.Payload)
+	req, err := http.NewRequest("POST", url, requestBody)
+	check(err)
+
+	if fixture.Event != nil {
+		req.Header.Add("X-Github-Event", *fixture.Event)
+	}
+	if fixture.Signature != nil {
+		req.Header.Add("X-Hub-Signature", *fixture.Signature)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	check(err)
+
+	return resp
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
 }
